9d881b946201781bd47e9474ccd3aa8d62f25de5
akzize <akzakariae@gmail.com>
2024-02-28 08:25:42 +0100

    Add session management and improve UI

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 import express, { json, urlencoded } from "express";
 import router from "./routes/userRouter.js";
+import dbConnect from "./database/config.js";
+import session from "express-session";
 
 const app = express();
+// connect to db
+dbConnect();
 
 // middlewares
 app.use(json());
 app.use(urlencoded({ extended: true }));
+// session
+app.use(
+	session({
+		secret: "authuser",
+		resave: false,
+		saveUninitialized: true,
+		cookie: { secure: false },
+	})
+);
+// engine
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.use('/', router)
+app.use("/", router);
+
+// 404 pages
+app.use((req, res) => {
+	res.send("<h1>404</h1><h3>Page not found</h3>");
+});
 
 const port = 3000;
 app.listen(port, () => {
